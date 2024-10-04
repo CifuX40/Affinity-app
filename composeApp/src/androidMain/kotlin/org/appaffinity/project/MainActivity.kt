51c8b42a1ef8981af8ad2357ac28820d0f00b4a4
Carlos Guerrero <ingenieria@affinityelectronics.com>
2024-10-04 13:34:07 +0200

    funcionamiento correcto

--- composeApp/src/androidMain/kotlin/org/appaffinity/project/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/appaffinity/project/MainActivity.kt
@@ -1,57 +1,22 @@
-package org.appaffinity.project
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import org.appaffinity.project.MenuUsuario
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Surface(color = MaterialTheme.colors.background) {
-                val navController = rememberNavController()
 
-                NavHost(navController = navController, startDestination = "menu_usuario") {
-                    composable("menu_usuario") {
-                        MenuUsuario(
-                            onNavigateToTecnico = { navController.navigate("menu_tecnico") },
-                            onNavigateToIdioma = { navController.navigate("idioma_screen") },
-                            onNavigateToFechaHora = { navController.navigate("ventana_fecha_hora") },
-                            onNavigateToTarifas = { navController.navigate("tarifa_screen") },
-                            onNavigateToFicha = { navController.navigate("ficha_screen") }
-                        )
-                    }
-                    composable("menu_tecnico") {
-                        MenuTecnico(
-                            onBack = { navController.navigateUp() },
-                            onNavigateToUsuario = { navController.navigate("menu_usuario") }
-                        )
-                    }
-                    composable("idioma_screen") {
-                        IdiomaScreen(
-                            onAceptarClick = { navController.navigateUp() }
-                        )
-                    }
-                    composable("ventana_fecha_hora") {
-                        FechaHora()
-                    }
-                    composable("tarifa_screen") {
-                        TarifaScreen(
-                            onClose = { navController.navigateUp() }
-                        )
-                    }
-                    composable("ficha_screen") {
-                        FichaScreen(
-                            onClose = { navController.navigateUp() }
-                        )
-                    }
-                }
-            }
+        setContent {
+            MenuUsuario()
         }
     }
+}
+
+@Preview
+@Composable
+fun AppAndroidPreview() {
+    MenuUsuario()
 }
--- composeApp/src/desktopMain/kotlin/org/appaffinity/project/main.kt
+++ composeApp/src/desktopMain/kotlin/org/appaffinity/project/main.kt
@@ -1,6 +1,5 @@
 package org.appaffinity.project
 
-import androidx.compose.runtime.*
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
