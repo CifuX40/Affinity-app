179bc9a8797c46bc21e6ed1ec5a8924d753d37b4
Carlos Guerrero <ingenieria@affinityelectronics.com>
2024-10-08 12:07:09 +0200

    8/10/2024

--- composeApp/src/commonMain/kotlin/org/appaffinity/project/Estilos.kt
+++ composeApp/src/commonMain/kotlin/org/appaffinity/project/Estilos.kt
@@ -1,13 +1,13 @@
 package org.appaffinity.project
 
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.material.Typography
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 // Definición de colores
@@ -15,49 +15,18 @@ val Negro = Color(0xFF1A171B)
 val AzulCian = Color(0xFF009EE0)
 val Blanco = Color(0xFFFFFFFF)
 val Naranja = Color(0xFFF5B130)
+val ColorBotones = Color(0xFF009EE0)
 
 // Tipografía
 val typography = Typography(
     defaultFontFamily = FontFamily.Default,
-    h1 = TextStyle(
-        fontWeight = FontWeight.Bold,
-        fontSize = 30.sp,
-        color = Blanco // Color del encabezado
-    ),
-    body1 = TextStyle(
-        color = Blanco,
-        fontSize = 16.sp
-    ),
-    button = TextStyle(
-        color = Blanco,
-        fontWeight = FontWeight.Bold,
-        fontSize = 16.sp
-    )
+    h1 = TextStyle(fontWeight = FontWeight.Bold, fontSize = 30.sp),
+    // Define otros estilos de texto aquí
 )
 
 // Formas
 val shapes = Shapes(
     small = RoundedCornerShape(4.dp),
-    medium = RoundedCornerShape(8.dp),
-    large = RoundedCornerShape(16.dp)
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
 )
-
-// Tema personalizado
-@Composable
-fun AppTheme(content: @Composable () -> Unit) {
-    MaterialTheme(
-        colors = lightColors(
-            primary = AzulCian,
-            secondary = Naranja,
-            background = Negro,
-            surface = Negro,
-            onPrimary = Blanco,
-            onSecondary = Blanco,
-            onBackground = Blanco,
-            onSurface = Blanco
-        ),
-        typography = typography,
-        shapes = shapes,
-        content = content
-    )
-}
